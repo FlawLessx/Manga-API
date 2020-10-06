639ec1cb849b16e902f01a7747f03ed6c057aadf
FlawLessx <adamherman14045@gmail.com>
2020-10-06 13:31:09 +0700

    test

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -8,7 +8,6 @@ var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
 
 exports.not_found = (req, res) => {
     res.status(404).json({
